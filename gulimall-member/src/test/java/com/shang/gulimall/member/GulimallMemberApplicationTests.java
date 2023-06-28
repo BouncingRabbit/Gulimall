a6964e9faa8d97cc5001feb03bb43c60c525c1ae
Asuka <1748401392@qq.com>
2023-06-28 16:04:15 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/shang/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shang.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
